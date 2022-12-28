d641c8749c86e7242bbfe37c96e9b7c56b450543
CamillaMogstad <camilla.mogstad@iths.se>
2022-12-28 19:59:05 +0100

    Removed readme

--- src/main.js
+++ src/main.js
@@ -1,30 +1,30 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-import './assets/main.css'
+import "./assets/main.css";
 
 /* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { library } from "@fortawesome/fontawesome-svg-core";
 
 /* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 /* import specific icons */
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import { faCircle as faCircleRegular } from '@fortawesome/free-regular-svg-icons'
-import { faCircle as faCircleSolid } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faCircle as faCircleRegular } from "@fortawesome/free-regular-svg-icons";
+import { faCircle as faCircleSolid } from "@fortawesome/free-solid-svg-icons";
 
-import VueScrollTo from 'vue-scrollto'
+import VueScrollTo from "vue-scrollto";
 
 /* add icons to the library */
-library.add(faLinkedin, faEnvelope, faCircleRegular, faCircleSolid)
+library.add(faLinkedin, faEnvelope, faCircleRegular, faCircleSolid);
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.component('font-awesome-icon', FontAwesomeIcon)
-app.use(router)
-app.use(VueScrollTo)
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.use(router);
+app.use(VueScrollTo);
 
-app.mount('#app')
+app.mount("#app");
